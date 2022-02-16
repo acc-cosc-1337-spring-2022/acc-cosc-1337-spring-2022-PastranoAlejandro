3bf64ab8ce6b7ffa56eb1b2da574064a76c2b565
PastranoAlejandro <alejandro.pastrano@g.austincc.edu>
2022-02-15 19:01:35 -0600

    class 2/15

--- src/examples/03_module/03_switch/main.cpp
+++ src/examples/03_module/03_switch/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include "switch.h"
+
 
 int main() 
 {
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
+#include"switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -24,4 +25,60 @@ TEST_CASE("Test get generation function")
 	REQUIRE(get_generation(1800) == "Invalid Year");
 
 
+}
+
+TEST_CASE("Test get option switch")
+{
+	REQUIRE("Option 1" == get_option(1));
+	REQUIRE("Option 2" == get_option(2));
+	REQUIRE("Option 3" == get_option(3));
+	REQUIRE("Option 4" == get_option(4));
+	REQUIRE("Invalid Option" == get_option(5));
+
+}
+
+
+TEST_CASE("Test boolean expressions")//logical operators NOT, AND, OR
+{
+	//Order of precedence !(not) &&(and) ||(or)
+	REQUIRE(true == ! (true && false));
+
+	//determine if a letter is a consonant
+	char letter = 'a';
+	REQUIRE(false == !(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'));
+
+	//determine if a number is in a range of 1 through 10
+	int min = 1, max = 10, num = 5;
+	REQUIRE(true == (num >= min && num <= max));
+
+	//determine if number is not in range with OR
+	num = 11;
+	REQUIRE(true == (num < min || num > max));
+
+	//determine if number is not in range with AND
+	REQUIRE(true == !(num >= min && num <= max));
+
+}
+
+TEST_CASE("Test AND truth table")
+{
+	REQUIRE(true == (true && true));
+	REQUIRE(false ==(true && false));
+	REQUIRE(false ==(false && true));
+	REQUIRE(false ==(false && false));//you can write out and in modern C++
+}
+
+TEST_CASE("Test OR truth table")
+{
+	REQUIRE(true == (true || true));
+	REQUIRE(true == (true || false));
+	REQUIRE(true == (false || true));
+	REQUIRE(false == (false || false));
+}
+
+TEST_CASE("Test NOT truth table")
+{
+	REQUIRE(false == !(true));
+	REQUIRE(true == !(false));
+	
 }

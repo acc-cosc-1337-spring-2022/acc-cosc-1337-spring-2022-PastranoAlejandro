a69bb5aacfa835afdcb08a0b1d7b90e8b6b105b9
PastranoAlejandro <alejandro.pastrano@g.austincc.edu>
2022-03-04 23:54:56 -0600

    homework 5 COMPLETED

--- src/classwork/05_assign/sequence.h
+++ src/classwork/05_assign/sequence.h
@@ -1,5 +1,35 @@
 //write include statements for string and vector
+#include<iostream>
+#include<string>
+#include<cctype>
 
+using std::cout; using std::cin; using std::string;
+
+
+//gc function
+double get_gc_content(const string& dna);
+
+//reverse string function
+
+std::string reverse_string(std::string dna);
+
+//dna complement function
+string get_dna_complement(string dna);
+
+
+void display_menu();
+
+void handle_menu(int option);
+
+std::string confirm_exit();
+
+void run_menu();
+
+void gc_choice();
+
+void dna_choice();
+
+bool check_input(string user_input);
 
 /*
 Write prototype for void function display_vector that accepts a parameter const reference vector of strings
--- test/homework_test/04_iteration_test/04_iteration_tests.cpp
+++ test/homework_test/04_iteration_test/04_iteration_tests.cpp
@@ -1,7 +1,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include"dna.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("TEST GC content function")
+{
+	REQUIRE(.375== get_gc_content("AGCTATAG"));
+}
